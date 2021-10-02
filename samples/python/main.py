841d005ff430158de79934afe5a2337c4e2360c2
Komal Soni <sk.26komal@gmail.com>
2021-10-03 02:18:01 +0530

    Update main.py

--- samples/python/main.py
+++ samples/python/main.py
@@ -17,10 +17,84 @@ def move(self):
     request.get_data()
     logger.info(request.json)
     while y<0 and x>0 and x<dims[0] and -y < (dims[-1]):
-        while(abs(state.y-y)<5 or abs(state.x-x)<5):
-            return moves['T']
-    return moves[random.randrange(len(moves))]
-
+        if (direction=="N") and abs(state.y-y)<5 or abs(state.x-x)<5:
+            if state.y > y and state.x == x:
+                moves['T']
+            elif state.y==y and state.x>x:
+                moves['R']
+                moves['T']
+            elif state.y==y and state.x<x:
+                moves['L']
+                moves['T']
+            elif state.y <y and state.x==x:
+                moves['R']
+                moves['R']
+                moves['T']
+            else:
+                moves['T']
+                moves[random.randrange(len(moves))]
+        else:
+            moves['F']
+            moves['T']
+        if (direction=="S") and abs(state.y-y)<5 or abs(state.x-x)<5:
+            if state.y > y and state.x ==x:
+                moves['L']
+                moves['L']
+                moves['T']
+            elif state.y ==y and state.x>x:
+                moves['R']
+                moves['T']
+            elif state.y==y and state.x<x:
+                moves['L']
+                moves['T']
+            elif state.y < y and state.x==x:
+                moves['T']
+            else:
+                moves['T']
+                moves[random.randrange(len(moves))]
+        else:
+            moves['F']
+            moves['T']
+        if (direction=="E") and abs(state.y-y)<5 or abs(state.x-x)<5:
+            if state.y > y and state.x = x:
+                moves['L']
+                moves['T']
+            elif state.y =y and state.x>x:
+                moves['T']
+            elif state.y =y and state.x<x:
+                moves['L']
+                moves['L']
+                moves['T']
+            elif state.y < y and state.x=x:
+                moves['L']
+                moves['T']
+            else:
+                moves['T']
+                moves[random.randrange(len(moves))]
+        else:
+            moves['F']
+            moves['T']
+        if (direction=="W") and abs(state.y-y)<5 or abs(state.x-x)<5:
+            if state.y > y and state.x = x:
+                moves['R']
+                moves['T']
+            elif state.y=y and state.x>x:
+                moves['R']
+                moves['R']
+                moves['T']
+            elif state.y =y and state.x<x:
+                moves['T']
+            elif state.y < y and state.x=x:
+                moves['R']
+                moves['T']
+            else:
+                moves['T']
+                moves[random.randrange(len(moves))]
+        else:
+            moves['F']
+            moves['T']
+        return moves[random.randrange(len(moves))]
+    
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
