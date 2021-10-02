262670127b800ef719d2bf9012758f4c3056ec32
Komal Soni <sk.26komal@gmail.com>
2021-10-03 01:40:23 +0530

    Update main.py

--- samples/python/main.py
+++ samples/python/main.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import random
+import math
 from flask import Flask, request
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
@@ -16,41 +17,84 @@ def move(self):
     request.get_data()
     logger.info(request.json)
     while y<0 and x>0 and x>dims[0] and -y < (dims[-1]):
-        while direction="N" and state.y>y:
-            return moves['T']
-        while direction="S" and state.y<y:
-            return moves['T']
-        while direction="W" and state.x<x:
-            return moves['T']
-        while direction="E" and state.x>x:
-            return moves['T']
-        
+        if (direction=="N") and abs(state.y-y)<5 and abs(state.x-x)<5:
+            if state.x > x and state.y = y:
+                moves['T']
+            elif state.x =x and state.y>y:
+                moves['R']
+                moves['T']
+            elif state.x =x and state.y<y:
+                moves['L']
+                moves['T']
+            elif state.x <x and state.y=y:
+                moves['R']
+                moves['R']
+                moves['T']
+            else:
+                moves[random.randrange(len(moves))]
+                moves['T']
+        else:
+            moves['F']
+            moves['T']
+        if (direction=="S") and abs(state.y-y)<5 and abs(state.x-x)<5:
+            if state.x > x and state.y = y:
+                moves['L']
+                moves['L']
+                moves['T']
+            elif state.x =x and state.y>y:
+                moves['R']
+                moves['T']
+            elif state.x =x and state.y<y:
+                moves['L']
+                moves['T']
+            elif state.x < x and logger.y=y:
+                moves['T']
+            else:
+                moves[random.randrange(len(moves))]
+                moves['T']
+        else:
+            moves['F']
+            moves['T']
+        if (direction=="E") and abs(state.y-y)<5 and abs(state.x-x)<5:
+            if state.x > x and state.y = y:
+                moves['L']
+                moves['T']
+            elif state.x =x and state.y>y:
+                moves['T']
+            elif state.x =x and state.y<y:
+                moves['L']
+                moves['L']
+                moves['T']
+            elif state.x < x and state.y=y:
+                moves['L']
+                moves['T']
+            else:
+                moves[random.randrange(len(moves))]
+                moves['T']
+        else:
+            moves['F']
+            moves['T']
+        if (direction=="W") and abs(state.y-y)<5 and abs(state.x-x)<5:
+            if state.x > x and state.y = y:
+                moves['R']
+                moves['T']
+            elif state.x =x and state.y>y:
+                moves['R']
+                moves['R']
+                moves['T']
+            elif state.x =x and state.y<y:
+                moves['T']
+            elif state.x < x and state.y=y:
+                moves['R']
+                moves['T']
+            else:
+                moves[random.randrange(len(moves))]
+                moves['T']
+        else:
+            moves['F']
+            moves['T']
         return moves[random.randrange(len(moves))]
-        
-        
-    '''
-    {
-  "_links": {
-    "self": {
-      "href": "https://YOUR_SERVICE_URL"
-    }
-  },
-  "arena": {
-    "dims": [4,3], // width, height
-    "state": {
-      "https://A_PLAYERS_URL": {
-        "x": 0, // zero-based x position, where 0 = left
-        "y": 0, // zero-based y position, where 0 = top
-        "direction": "N", // N = North, W = West, S = South, E = East
-        "wasHit": false,
-        "score": 0
-      }
-      ... // also you and the other players
-    }
-  }
-}
 
-    #return '''
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
