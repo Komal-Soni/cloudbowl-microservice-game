3e1b39d36acca3244e9a9be15ec7e3b100f2366c
Komal Soni <sk.26komal@gmail.com>
2021-10-03 03:02:33 +0530

    Update main.py

--- samples/python/main.py
+++ samples/python/main.py
@@ -16,10 +16,65 @@ def move(self):
     logger.info(request.json)
     x=self.x
     y=self.y
-    while(state.x in x+i for i in range(-4,5) and state.y in y+i for i in range(-4,5)):
-        return moves[1]
-    return moves[random.randrange(len(moves))]
-    
+    for x,-y in dims:
+        if (direction=="N") and abs(state.y-y)<5 or abs(state.x-x)<5:
+            if state.y > y and state.x == x:
+                return moves[1]
+            elif state.y==y and state.x>x:
+                direction=="E"
+                return moves[1]
+            elif state.y==y and state.x<x:
+                direction=="W"
+                return moves[1]
+            elif state.y <y and state.x==x:
+                direction=="S"
+                return moves[1]
+            else:
+                return moves[random.randrange(len(moves))]
+        elif (direction=="S") and abs(state.y-y)<5 or abs(state.x-x)<5:
+            if state.y > y and state.x ==x:
+                direction=="N"
+                return moves[1]
+            elif state.y ==y and state.x>x:
+                direction=="E"
+                return moves[1]
+            elif state.y==y and state.x<x:
+                direction=="W"
+                return moves[1]
+            elif state.y < y and state.x==x:
+                return moves[1]
+            else:
+                return moves[random.randrange(len(moves))]
+        elif (direction=="E") and abs(state.y-y)<5 or abs(state.x-x)<5:
+            if state.y > y and state.x = x:
+                direction=="N"
+                return moves[1]
+            elif state.y =y and state.x>x:
+                return moves[1]
+            elif state.y =y and state.x<x:
+                direction=="W"
+                return moves[1]
+            elif state.y < y and state.x=x:
+                direction=="S"
+                return moves[1]
+            else:
+                return moves[random.randrange(len(moves))]
+        elif (direction=="W") and abs(state.y-y)<5 or abs(state.x-x)<5:
+            if state.y > y and state.x = x:
+                direction=="N"
+                return moves[1]
+            elif state.y=y and state.x>x:
+                direction=="E"
+                return moves[1]
+            elif state.y =y and state.x<x:
+                return moves[1]
+            elif state.y < y and state.x=x:
+                direction=="S"
+                return moves[1]
+            else:
+                return moves[random.randrange(len(moves))]
+        else:
+            return moves[random.randrange(len(moves))]
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
