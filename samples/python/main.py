3bcdcc70c6beaee63ebfccda98df7a8923680fc1
Komal Soni <sk.26komal@gmail.com>
2021-10-02 22:32:05 +0530

    Update main.py

--- samples/python/main.py
+++ samples/python/main.py
@@ -107,6 +107,45 @@ def move():
     else:
         moves['F']
         moves['T']
+    return moves[random.randrange(len(moves))]
+'''def Move(self, Direction):              #This function is how a character moves around in a certain direction
+
+        if Direction == "UP":
+            if self.Row > 0:                #If within boundaries of grid
+                if self.CollisionCheck("UP") == False:       #And nothing in the way
+                   self.Row -= 1            #Go ahead and move
+
+        elif Direction == "LEFT":
+            if self.Column > 0:
+                if self.CollisionCheck("LEFT") == False:
+                    self.Column -= 1
+
+        elif Direction == "RIGHT":
+            if self.Column < MapSize-1:
+                if self.CollisionCheck("RIGHT") == False:
+                         self.Column += 1
+
+        elif Direction == "DOWN":
+            if self.Row < MapSize-1:
+                if self.CollisionCheck("DOWN") == False:
+                    self.Row += 1
+
+        Map.update()       
+
+    def CollisionCheck(self, Direction):       #Checks if anything is on top of the grass in the direction that the character wants to move. Used in the move function
+        if Direction == "UP":
+            if len(Map.Grid[self.Column][(self.Row)-1]) > 1:
+                return True
+        elif Direction == "LEFT":
+            if len(Map.Grid[self.Column-1][(self.Row)]) > 1:
+                return True
+        elif Direction == "RIGHT":
+            if len(Map.Grid[self.Column+1][(self.Row)]) > 1:
+                return True
+        elif Direction == "DOWN":
+            if len(Map.Grid[self.Column][(self.Row)+1]) > 1:
+                return True
+        return False'''
    
     '''
     {
@@ -130,7 +169,7 @@ def move():
   }
 }
 
-    #return moves[random.randrange(len(moves))]'''
+    #return '''
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
