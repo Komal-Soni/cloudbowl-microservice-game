8e054dc275822ad6f1a12b8992ccd0b89e9a39f5
Komal Soni <sk.26komal@gmail.com>
2021-10-03 04:26:31 +0530

    Update main.py

--- samples/python/main.py
+++ samples/python/main.py
@@ -2,6 +2,7 @@ import os
 import logging
 import random
 import math
+import json
 from flask import Flask, request
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
@@ -16,66 +17,48 @@ def move(self):
     logger.info(request.json)
     x=self.x
     y=self.y
-    while True:
-        if (direction=="N") and (abs(state.y-y)<5 or abs(state.x-x)<5):
-            if state.y > y and state.x == x:
-                return moves[1]
-            elif state.y==y and state.x>x:
-                direction=="E"
-                return moves[1]
-            elif state.y==y and state.x<x:
-                direction=="W"
-                return moves[1]
-            elif state.y <y and state.x==x:
-                direction=="S"
-                return moves[1]
-            else:
-                return moves[random.randrange(len(moves))]
-        elif (direction=="S") and (abs(state.y-y)<5 or abs(state.x-x)<5):
-            if state.y > y and state.x ==x:
-                direction=="N"
-                return moves[1]
-            elif state.y ==y and state.x>x:
-                direction=="E"
-                return moves[1]
-            elif state.y==y and state.x<x:
-                direction=="W"
-                return moves[1]
-            elif state.y < y and state.x==x:
-                return moves[1]
-            else:
-                return moves[random.randrange(len(moves))]
-        elif (direction=="E") and (abs(state.y-y)<5 or abs(state.x-x)<5):
-            if state.y > y and state.x = x:
-                direction=="N"
-                return moves[1]
-            elif state.y =y and state.x>x:
-                return moves[1]
-            elif state.y =y and state.x<x:
-                direction=="W"
-                return moves[1]
-            elif state.y < y and state.x=x:
-                direction=="S"
-                return moves[1]
-            else:
-                return moves[random.randrange(len(moves))]
-        elif (direction=="W") and (abs(state.y-y)<5 or abs(state.x-x)<5):
-            if state.y > y and state.x = x:
-                direction=="N"
-                return moves[1]
-            elif state.y=y and state.x>x:
-                direction=="E"
-                return moves[1]
-            elif state.y =y and state.x<x:
-                return moves[1]
-            elif state.y < y and state.x=x:
-                direction=="S"
-                return moves[1]
-            else:
-                return moves[random.randrange(len(moves))]
-        else:
-            return moves[random.randrange(len(moves))]
-
+    d=self.direction
+    json_object=json.load(logger.info(request.json))
+    ex=json_object[0]["state"]["x"]
+    ey=json_object[0]["state"]["y"]
+    edir=json_object[0]["state"]["direction"]
+    if((ex in range(x-4,x+5) or (ey in range(y-4,y+5))):
+       i=1
+       return moves[i]
+    elif:
+       if(edir=="N" and ex==x and ey<y):
+          i=2       
+       elif(edir=="S" and ex==x and ey>y):
+          i=2
+       elif(edir=="E" and ex<x and ey=y):
+          i=2
+       elif(edir=="W" and ex>x and ey=y):
+          i=3
+       else:
+          i=0
+       return moves[i]
+    elif:       
+       if d="S" and y==dims[-1] and x==0:
+          i=2
+       elif d="S" and y==dims[-1] and x==dims[0]:
+          i=3
+       elif d="E" and y==dims[-1] and x==dims[0]:
+          i=2
+       elif d="E" and y==0 and x==dims[0]:
+          i=3
+       elif d="W" and y==0 and x==0:
+          i=2
+       elif d="W" and y==dims[-1] and x==0:
+          i=3
+       elif d="N" and y==0 and x==0:
+          i=3
+       elif d="N" and y==0 and x==dims[0]:
+          i=2
+       else:
+          i=0
+       return moves[i]
+    else:
+       return moves[random.randrange(len(moves))]
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
   
