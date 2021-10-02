d96964bde84bc2ad48ec01c6cd55a3861bead699
Komal Soni <sk.26komal@gmail.com>
2021-10-03 00:25:36 +0530

    Update main.py

--- samples/python/main.py
+++ samples/python/main.py
@@ -1,18 +1,3 @@
-
-# Copyright 2020 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import os
 import logging
 import random
@@ -28,8 +13,17 @@ moves = ['F', 'T', 'L', 'R']
 def move():
     request.get_data()
     logger.info(request.json)
-    
-    return moves[random.randrange(len(moves))]
+    while y<0 and x>0:
+        if direction="N" and logger.y>y:
+            return moves['T']
+        if direction="S" and logger.y<y:
+            return moves['T']
+        if direction="W" and logger.x<x:
+            return moves['T']
+        if direction="E" and logger.x>x:
+            return moves['T']
+        else:
+            return moves[random.randrange(len(moves))]
         
         
     '''
