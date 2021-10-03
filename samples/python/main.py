474e50ee14e339c03d32eadfa23c48873f68ae39
Komal Soni <sk.26komal@gmail.com>
2021-10-03 07:55:15 +0530

    Update main.py

--- samples/python/main.py
+++ samples/python/main.py
@@ -10,73 +10,73 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 moves = ['F', 'T', 'L', 'R']
-def my_data(all_data):
-    my_name = all_data['_links']['self']['href']
-    my_coord = all_data["arena"]["state"][my_name]
-
-    return my_coord, my_name
-
-def get_command(my_direction, my_coord_x, my_coord_y, arena_x, arena_y, all_user_data):
-    if my_direction == "N":
-        # X axis constant
-        # Y decreases as we move forward
-        if my_coord_y == 0:
-            return "R"
-        elif is_someone_present([my_coord_x], [my_coord_y-1, my_coord_y-2, my_coord_y-3], all_user_data):
-            return "T"
-        else:
-            return "F"
-
-    elif my_direction == "S":
-        # X axis constant
-        # Y increases as we move forward
-        if my_coord_y == arena_y - 1:
-            return "R"
-        elif is_someone_present([my_coord_x], [my_coord_y+1, my_coord_y+2, my_coord_y+3], all_user_data):
-            return "T"
-        else:
-            return "F"
-
-    elif my_direction == "E":
-        # Y axis constant
-        # X increases as we move forward
-        if my_coord_x == arena_x - 1:
-            return "R"
-        elif is_someone_present([my_coord_x+1, my_coord_x+2, my_coord_x+3], [my_coord_y], all_user_data):
-            return "T"
-        else:
-            return "F"
-
-    elif my_direction == "W":
-        # Y axis constant
-        # X decreases as we move forward
-        if my_coord_x == 0:
-            return "R"
-        elif is_someone_present([my_coord_x-1, my_coord_x-2, my_coord_x-3], [my_coord_y], all_user_data):
-            return "T"
-        else:
-            return "F"
-    else:
-        return moves[random.randrange(len(moves))]
-
-
-def is_someone_present(possible_points_x, possible_points_y, all_user_data):
-    for _each_user_name, each_user_stats in all_user_data.items():
-        if each_user_stats["x"] in possible_points_x and each_user_stats["y"] in possible_points_y:
-            return True
-    else:
-        return False
-
 
 @app.route("/", methods=['POST'])
-def move():
+def move(self):
     request.get_data()
     logger.info(request.json)
-    return moves[random.randrange(len(moves))]
-    my_coord, my_name = my_data(request.json)
-    arena_x, arena_y = request.json["arena"]["dims"]
-    return get_command(my_coord['direction'], my_coord['x'], my_coord['y'], arena_x, arena_y, request.json["arena"]["state"])
-
+    x=0
+    y=0
+    d="N"
+    ns={}
+    s={}
+    json.load(logger.info(request.json))
+    xaxis=request.json["arena"]["dims"][0]
+    yaxis=request.json["arena"]["dims"][1]
+    for player in request.json["arena"]["state"]:
+        ex=request.json["arena"]["state"]["x"]
+        ey=request.json["arena"]["state"]["y"]
+        edir=request.json["arena"]["state"]["direction"]
+        if( y==ey && math.abs(x - ex)<=3):
+            ns.append(player)
+    	if( x==ex && math.abs(y - ey)<=3):
+    		s.append(player)
+        distance_sq = math.pow( math.abs(x-ex), 2) + math.pow(math.abs(y- ey), 2)
+//      distance = math.sqrt(distance_sq)
+        while (distance<4 or distance==4):
+            if(d=="E" and y=ey and ex-x<4 and ex>x):
+                return "T"
+            elif(d=="W" and y=ey and x-ex<4 and x>ex):
+                return "T"
+            elif(d=="N" and x=ex and y-ey<4 and y>ey):
+                return "T"
+            elif(d=="S" and x=ex and ey-y<4 and ey>y):
+                return "T"
+            else:
+                move= "F"
+                if d=="N":
+                    for plyr in ns:
+                        if d=="N" and plyr.x<x:
+                            move="L"
+                        if d=="N" and plyr.x>x:
+                            move="R"
+                        if d=="S" and plyr.x<x:
+                            move="R"
+                        if d=="S" and plyr.x>x:
+                            move="L"
+                        if move!="F":
+                            return move
+                if d=="S" and yaxis-y<4:
+                        if xaxis/2>x:
+                            move="L"
+                        else:
+                            move="R"
+                if d=="N" and y<4:
+                        if xaxis/2>x:
+                            move="R"
+                        else:
+                            move="L"                            
+                if d=="W" and x<4:
+                        if yaxis/2>y:
+                            move="L"
+                        else:
+                            move="R"            
+                if d=="E" and xaxis-x<4:
+                        if yaxis/2>y:
+                            move="R"
+                        else:
+                            move="L"            
+                        return move
+    return moves[random.randrange(len(moves))]           
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
-  
